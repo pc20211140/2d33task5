f3535388b8733738ab59290da6c90add103a4212
pc20211140 <115052204+pc20211140@users.noreply.github.com>
2022-10-26 09:01:30 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,29 @@
+input.onButtonPressed(Button.A, function () {
+    while (xvalue <= 4) {
+        while (xvalue <= 4) {
+            led.plot(xvalue, yvalue)
+            xvalue += 1
+            basic.pause(500)
+        }
+        yvalue += 1
+        xvalue = 0
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 5; index++) {
+        for (let index = 0; index < 5; index++) {
+            led.plot(xvalue, yvalue)
+            basic.pause(200)
+            xvalue += 1
+        }
+        yvalue += 1
+        xvalue = 0
+    }
+})
+let yvalue = 0
 let xvalue = 0
-while (xvalue <= 5) {
-    led.plot(xvalue, 0)
-    xvalue += 1
-    basic.pause(500)
-}
+xvalue = 0
+yvalue = 0
 basic.forever(function () {
 	
 })
